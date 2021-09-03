5ab5d1138f3d7131bd1b26f58cb0f03d1e9a9882
tekoszi <tekoszi@hotmail.com>
2021-09-03 21:03:36 +0200

    some date operations, month change buttons, ideas for local storage, db.json

--- app.py
+++ app.py
@@ -1,39 +1,60 @@
-from flask import Flask, request, redirect, session, render_template, flash
+import json
+import os
+from datetime import datetime
 
+from flask import Flask, render_template, request
+from calendar import monthrange, weekday
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET", "POST"])
 def main():
-    # def humiditySensor():
-    #     GPIO.setwarnings(True)
-    #     GPIO.setmode(GPIO.BCM)
-    #     instance = dht11.DHT11(pin=4)
-    #     result = instance.read()
-    #     if result.is_valid():
-    #         data = (str(datetime.datetime.now()), str(result.temperature) + ' ℃', str(result.humidity) + '%')
-    #         return data
-    #     else:
-    #         result = instance.read()
-    #         if result.is_valid():
-    #             data = (str(datetime.datetime.now()), str(result.temperature) + ' ℃', str(result.humidity) + '%')
-    #             return data
-    #         else:
-    #             if result.is_valid():
-    #                 data = (
-    #                 str(datetime.datetime.now()), str(result.temperature) + ' ℃', str(result.humidity) + '%')
-    #                 return data
-    #             else:
-    #                 sleep(0.5)
-    #                 data = ('no date', 'no temperature', 'no humidity')
-    #                 return data
-    # return render_template('plants.html', data=humiditySensor(), plants=db['plants'], water=db['water'],
-    #                        alan=db['alan'])
+    global c
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, 'db.json')
+    current_month = datetime.today().month
+    current_year = datetime.today().year
+    current_day = datetime.today().day
+    # keep those variables in localstorage
+    # allways when home page is run those should be writen from localstorage
+    #localStorage.setItem(key, value);
+    #localStorage.getItem(key)
 
+    # we should clear local storage for current month to be default when you run app again.
+    #
+    nameOfWeekDay = weekday(current_year, current_month, current_day)
+    print(current_month, current_year, nameOfWeekDay)
+    def loadDB():
+        with open(filename, mode='r') as json_file:
+            db = json.load(json_file)
+            return db
+
+    def witeDB(db):
+        with open(filename, mode='w') as json_file:
+            json.dump(db, json_file)
+
+    db = loadDB()
+
+    return render_template('main.html', people=db['people'], nameOfWeekDay=db["weekdays"][nameOfWeekDay], nameOfMonth=db["months"][current_month-1])
+
+
+@app.route("/calculateMonthDays", methods=["GET", "POST"])
+def previousMonth():
+    # decrement current month
+    # keep those variables in localstorage
+    # allways when home page is run those should be writen from localstorage
+    # localStorage.setItem(key, value);
+    # localStorage.getItem(key)
     return render_template('main.html')
 
-
-
+def nextMonth():
+    # increment current month
+    # keep those variables in localstorage
+    # allways when home page is run those should be writen from localstorage
+    # localStorage.setItem(key, value);
+    # localStorage.getItem(key)
+    return render_template('main.html')
+    # return render_template('main.html', monthrange=monthrange(year, month))
 
 
 if __name__ == "__main__":
